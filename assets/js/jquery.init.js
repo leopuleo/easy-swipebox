9491215d43154fb52a8980b4f6c7a67bb74fbd18
leopuleo <leonardo.giacone@gmail.com>
2015-04-14 01:38:11 +0200

    Bug fix: Added JPEG extension support Bug fix: Use image title as link title (alt fallback)

--- assets/js/jquery.init.js
+++ assets/js/jquery.init.js
@@ -3,16 +3,23 @@
 
 		// Add class swipebox to all links to images
 		$('a[href]').filter(function() {
-			return /(jpg|gif|png)$/.test( $(this).attr('href'))
+			return /(jpg|jpeg|gif|png)$/.test( $(this).attr('href'))
 		}).addClass("swipebox");
 
 		// Add class swipebox to all links to videos
 		$('a[href*="youtube.com"]').addClass('swipebox-video');
 		$('a[href*="vimeo.com"]').addClass('swipebox-video');
 
-		// If link has not title, add img alt as title to links
+		// If link has not title, add img title/alt as title to links
 		$('a.swipebox').filter(function(){
-			var title_img = $(this).find('img').attr('alt');
+
+			// Check if image has title - else alt
+			if ($(this).find('img').attr('title')){
+				var title_img = $(this).find('img').attr('title');
+			} else {
+				var title_img = $(this).find('img').attr('alt');
+			}
+
 			if ($(this).not("[title]")){
 				$(this).attr('title', title_img);
 			}
