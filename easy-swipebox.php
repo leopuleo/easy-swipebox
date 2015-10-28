b7ab311787a509716922c144545a6c1b68ff22bf
leopuleo <leonardo.giacone@gmail.com>
2015-10-28 01:12:26 +0100

    new versione 1.0.1

--- easy-swipebox.php
+++ easy-swipebox.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/leopuleo
 Description: This plugin enable <a href="http://brutaldesign.github.io/swipebox/">SwipeBox jQuery extension</a> on all links to image or Video (Youtube / Vimeo).
 Text Domain: easy-swipebox
 Domain Path: languages
-Version: 1.0
+Version: 1.0.1
 Author: Leonardo Giacone
 Author URI: https://github.com/leopuleo
 */
