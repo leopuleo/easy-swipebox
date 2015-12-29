44ea46342305e4863e64771f91815d7b5940c31b
leopuleo <leonardo.giacone@gmail.com>
2015-12-29 01:18:18 +0100

    code styling

--- admin/class-easy-swipebox-admin.php
+++ admin/class-easy-swipebox-admin.php
@@ -102,7 +102,7 @@ class EasySwipeboxAdmin {
       apply_filters($this->plugin_name . '-settings-page-title', __('Easy SwipeBox', $this->plugin_name)),
       'install_plugins',
       $this->plugin_name . '-settings',
-      array($this, 'easySwipeBox_plugin_settings_page')
+      array($this, 'easySwipeBoxSettingsPage')
     );
   }
 
@@ -115,8 +115,8 @@ class EasySwipeboxAdmin {
   public function settingsInit() {
 
     // Register Settings
-    register_setting('easySwipeBox_autodetect', 'easySwipeBox_autodetect', array($this, 'sanitize_autodetect'));
-    register_setting('easySwipeBox_lightbox', 'easySwipeBox_lightbox', array($this, 'sanitize_lightbox'));
+    register_setting('easySwipeBox_autodetect', 'easySwipeBox_autodetect', array($this, 'sanitizeAutodetect'));
+    register_setting('easySwipeBox_lightbox', 'easySwipeBox_lightbox', array($this, 'sanitizeLightbox'));
     register_setting('easySwipeBox_overview', 'easySwipeBox_overview');
 
     // Section: General Settings
@@ -285,7 +285,7 @@ class EasySwipeboxAdmin {
     ?>
     <label>
       <input type="hidden" id="hidden_easySwipeBox_autodetect[video]" name="easySwipeBox_autodetect[video]" value="0" />
-      <input id="easySwipeBox_autodetect[video]" type="checkbox" name="easySwipeBox_autodetect[video]" value="1" <?php if ($this->options_autodetect['video'] == 1) {echo 'checked="checked"'}; ?> />
+      <input id="easySwipeBox_autodetect[video]" type="checkbox" name="easySwipeBox_autodetect[video]" value="1" <?php if ($this->options_autodetect['video'] == 1) {echo 'checked="checked"';} ?> />
       <?php _e('Add SwipeBox to video links by default', $this->plugin_name); ?>
         <em>(<?php _e('Youtube / Vimeo', $this->plugin_name); ?>)</em>
     </label>
@@ -312,8 +312,8 @@ class EasySwipeboxAdmin {
   public function animationRender() {
     ?>
     <label>
-      <input id="easySwipeBox_lightbox[useCSS]" type="radio" name="easySwipeBox_lightbox[useCSS]" value="1" <?php if ($this->options_lightbox['useCSS'] == 1) {echo 'checked="checked"'}; ?> /><?php _e('CSS', $this->plugin_name); ?>
-      <input id="easySwipeBox_lightbox[useJquery]" type="radio" name="easySwipeBox_lightbox[useCSS]" value="0" <?php if ($this->options_lightbox['useCSS'] == 0) {echo 'checked="checked"'}; ?> /><?php _e('Jquery', $this->plugin_name); ?><br>
+      <input id="easySwipeBox_lightbox[useCSS]" type="radio" name="easySwipeBox_lightbox[useCSS]" value="1" <?php if ($this->options_lightbox['useCSS'] == 1) {echo 'checked="checked"';} ?> /><?php _e('CSS', $this->plugin_name); ?>
+      <input id="easySwipeBox_lightbox[useJquery]" type="radio" name="easySwipeBox_lightbox[useCSS]" value="0" <?php if ($this->options_lightbox['useCSS'] == 0) {echo 'checked="checked"';} ?> /><?php _e('Jquery', $this->plugin_name); ?><br>
       <em><?php _e('Select the method used to render the lightbox. Use Jquery if you are having problems with old browsers (Default: CSS).', $this->plugin_name); ?></em>
     </label>
     <?php
@@ -323,7 +323,7 @@ class EasySwipeboxAdmin {
     ?>
     <label>
       <input type="hidden" id="hidden_easySwipeBox_lightbox[useSVG]" name="easySwipeBox_lightbox[useSVG]" value="0" />
-      <input id="easySwipeBox_lightbox[useSVG]" type="checkbox" name="easySwipeBox_lightbox[useSVG]" value="1" <?php if ($this->options_lightbox['useSVG'] == 1) {echo 'checked="checked"'}; ?> />
+      <input id="easySwipeBox_lightbox[useSVG]" type="checkbox" name="easySwipeBox_lightbox[useSVG]" value="1" <?php if ($this->options_lightbox['useSVG'] == 1) {echo 'checked="checked"';} ?> />
       <strong><?php _e('Use SVG Icons', $this->plugin_name); ?></strong><br>
       <em><?php _e('Disable this option if you are having problems with navigation icons not visible on some devices (Default: true).', $this->plugin_name); ?></em>
     </label>
@@ -334,7 +334,7 @@ class EasySwipeboxAdmin {
     ?>
     <label>
       <input type="hidden" id="hidden_easySwipeBox_lightbox[removeBarsOnMobile]" name="easySwipeBox_lightbox[removeBarsOnMobile]" value="0" />
-      <input id="easySwipeBox_lightbox[removeBarsOnMobile]" type='checkbox' name='easySwipeBox_lightbox[removeBarsOnMobile]' value='1' <?php if ($this->options_lightbox['removeBarsOnMobile'] == 1) {echo 'checked="checked"'}; ?> />
+      <input id="easySwipeBox_lightbox[removeBarsOnMobile]" type='checkbox' name='easySwipeBox_lightbox[removeBarsOnMobile]' value='1' <?php if ($this->options_lightbox['removeBarsOnMobile'] == 1) {echo 'checked="checked"';} ?> />
       <strong><?php _e('Hide navigation bar on mobile', $this->plugin_name); ?></strong><br>
       <em><?php _e('Select this options if you like to hide the navigation bar on mobile devices (Default: true).', $this->plugin_name); ?></em>
     </label>
@@ -345,7 +345,7 @@ class EasySwipeboxAdmin {
     ?>
     <label>
       <input type="hidden" id="hidden_easySwipeBox_lightbox[hideCloseButtonOnMobile]" name="easySwipeBox_lightbox[hideCloseButtonOnMobile]" value="0" />
-      <input id="easySwipeBox_lightbox[hideCloseButtonOnMobile]" type='checkbox' name='easySwipeBox_lightbox[hideCloseButtonOnMobile]' value='1' <?php if ($this->options_lightbox['hideCloseButtonOnMobile'] == 1) {echo 'checked="checked"'}; ?> />
+      <input id="easySwipeBox_lightbox[hideCloseButtonOnMobile]" type='checkbox' name='easySwipeBox_lightbox[hideCloseButtonOnMobile]' value='1' <?php if ($this->options_lightbox['hideCloseButtonOnMobile'] == 1) {echo 'checked="checked"';} ?> />
       <strong><?php _e('Hide close button on mobile', $this->plugin_name); ?></strong><br>
       <em><?php _e('Select this options if you like to hide the close button on mobile devices (Default: true).', $this->plugin_name); ?></em>
     </label>
@@ -385,7 +385,7 @@ class EasySwipeboxAdmin {
     ?>
     <label>
       <input type="hidden" id="hidden_easySwipeBox_lightbox[loopAtEnd]" name="easySwipeBox_lightbox[loopAtEnd]" value="0" />
-      <input id="easySwipeBox_lightbox[loopAtEnd]" type="checkbox" name="easySwipeBox_lightbox[loopAtEnd]" value="1" <?php if ($this->options_lightbox['loopAtEnd'] == 'true') {echo 'checked="checked"'}; ?> />
+      <input id="easySwipeBox_lightbox[loopAtEnd]" type="checkbox" name="easySwipeBox_lightbox[loopAtEnd]" value="1" <?php if ($this->options_lightbox['loopAtEnd'] == 'true') {echo 'checked="checked"';} ?> />
       <strong><?php _e('Loop', $this->plugin_name); ?></strong><br>
       <em><?php _e('Select this options if you like to loop back to the first image after the last is reached (Default: false).', $this->plugin_name); ?></em>
     </label>
@@ -396,7 +396,7 @@ class EasySwipeboxAdmin {
     ?>
     <label>
       <input type="hidden" id="hidden_easySwipeBox_lightbox[autoplayVideos]" name="easySwipeBox_lightbox[autoplayVideos]" value="0" />
-      <input id="easySwipeBox_lightbox[autoplayVideos]" type='checkbox' name='easySwipeBox_lightbox[autoplayVideos]' value='1' <?php if ($this->options_lightbox['autoplayVideos'] == 1) {echo 'checked="checked"'}; ?> />
+      <input id="easySwipeBox_lightbox[autoplayVideos]" type='checkbox' name='easySwipeBox_lightbox[autoplayVideos]' value='1' <?php if ($this->options_lightbox['autoplayVideos'] == 1) {echo 'checked="checked"';} ?> />
       <strong><?php _e('Play video at opening', $this->plugin_name); ?></strong><br>
       <em><?php _e('Select this options if you like to autoplay video at opening (Default: false).', $this->plugin_name); ?></em>
     </label>
@@ -441,52 +441,50 @@ class EasySwipeboxAdmin {
    * @since    1.1
    * @access   public
    */
-  public function easySwipeBox_plugin_settings_page() {
+  public function easySwipeBoxSettingsPage() {
 
     ?>
     <form method="post" action="options.php">
       <div class="wrap">
       <h2><?php _e('Easy SwipeBox Settings', $this->plugin_name); ?></h2>
 
-      <?php
-            if(isset($_GET['tab'])){
-                $active_tab = $_GET['tab'];
-            } else {
-              $active_tab = 'general_options';
-            }
-            ?>
+    <?php
+      if (isset($_GET['tab'])) {
+          $active_tab = $_GET['tab'];
+      } else {
+        $active_tab = 'general_options';
+      }
+    ?>
 
-            <h2 class="nav-tab-wrapper">
-                <a href="<?php echo admin_url('options-general.php?page=easy-swipebox-settings&tab=general_options');?>" class="nav-tab <?php echo $active_tab == 'general_options' ? 'nav-tab-active' : ''; ?>"><?php _e('General Settings', $this->plugin_name); ?></a>
-                <a href="<?php echo admin_url('options-general.php?page=easy-swipebox-settings&tab=lightbox_options');?>" class="nav-tab <?php echo $active_tab == 'lightbox_options' ? 'nav-tab-active' : ''; ?>"><?php _e('SwipeBox Settings', $this->plugin_name); ?></a>
-                <a href="<?php echo admin_url('options-general.php?page=easy-swipebox-settings&tab=overview');?>" class="nav-tab <?php echo $active_tab == 'overview' ? 'nav-tab-active' : ''; ?>"><?php _e('Overview', $this->plugin_name); ?></a>
-            </h2>
+    <h2 class="nav-tab-wrapper">
+        <a href="<?php echo admin_url('options-general.php?page=easy-swipebox-settings&tab=general_options');?>" class="nav-tab <?php echo $active_tab == 'general_options' ? 'nav-tab-active' : ''; ?>"><?php _e('General Settings', $this->plugin_name); ?></a>
+        <a href="<?php echo admin_url('options-general.php?page=easy-swipebox-settings&tab=lightbox_options');?>" class="nav-tab <?php echo $active_tab == 'lightbox_options' ? 'nav-tab-active' : ''; ?>"><?php _e('SwipeBox Settings', $this->plugin_name); ?></a>
+        <a href="<?php echo admin_url('options-general.php?page=easy-swipebox-settings&tab=overview');?>" class="nav-tab <?php echo $active_tab == 'overview' ? 'nav-tab-active' : ''; ?>"><?php _e('Overview', $this->plugin_name); ?></a>
+    </h2>
 
-        <?php
+    <?php
+      switch ($active_tab) {
+        case 'general_options':
+          settings_fields('easySwipeBox_autodetect');
+          do_settings_sections('easySwipeBox_autodetect');
+          submit_button();
+          break;
 
-        switch ($active_tab) {
-          case 'general_options':
-            settings_fields('easySwipeBox_autodetect');
-            do_settings_sections('easySwipeBox_autodetect');
-            submit_button();
-            break;
+        case 'lightbox_options':
+          settings_fields('easySwipeBox_lightbox');
+          do_settings_sections('easySwipeBox_lightbox');
+          submit_button();
+          break;
 
-          case 'lightbox_options':
-            settings_fields('easySwipeBox_lightbox');
-            do_settings_sections('easySwipeBox_lightbox');
-            submit_button();
-            break;
+        case 'overview':
+          settings_fields('easySwipeBox_overview');
+          do_settings_sections('easySwipeBox_overview');
+          break;
 
-          case 'overview':
-            settings_fields('easySwipeBox_overview');
-            do_settings_sections('easySwipeBox_overview');
-            break;
-
-          default:
-            break;
-        }
-
-        ?>
+        default:
+          break;
+      }
+    ?>
       </div>
     </form>
   <?php
@@ -498,18 +496,20 @@ class EasySwipeboxAdmin {
    * @since    1.1
    * @access   public
    */
-  public function sanitize_autodetect($input) {
+  public function sanitizeAutodetect($input) {
     $valid_input = array();
 
-    if( isset($input['image']))
-        $valid_input['image'] = (bool)($input['image']);
+    if(isset($input['image'])) {
+      $valid_input['image'] = (bool)($input['image']);
+    }
 
-    if( isset( $input['video']))
-        $valid_input['video'] = (bool)($input['video']);
+    if(isset( $input['video'])) {
+      $valid_input['video'] = (bool)($input['video']);
+    }
 
-    if( isset( $input['class_exclude']))
-        $valid_input['class_exclude'] = sanitize_text_field($input['class_exclude']);
-
+    if(isset( $input['class_exclude'])) {
+      $valid_input['class_exclude'] = sanitize_text_field($input['class_exclude']);
+    }
     return $valid_input;
   }
 
@@ -519,52 +519,53 @@ class EasySwipeboxAdmin {
    * @since    1.1
    * @access   public
    */
-  public function sanitize_lightbox($input) {
-        $valid_input = array();
+  public function sanitizeLightbox($input) {
+    $valid_input = array();
 
-        if(isset($input['useCSS']))
-            $valid_input['useCSS'] = (bool)($input['useCSS']);
+    if(isset($input['useCSS'])) {
+      $valid_input['useCSS'] = (bool)($input['useCSS']);
+    }
+    if(isset($input['useSVG'])) {
+      $valid_input['useSVG'] = (bool)($input['useSVG']);
+    }
+    if(isset($input['removeBarsOnMobile'])) {
+      $valid_input['removeBarsOnMobile'] = (bool)($input['removeBarsOnMobile']);
+    }
+    if(isset( $input['hideCloseButtonOnMobile'])){
+      $valid_input['hideCloseButtonOnMobile'] = (bool)($input['hideCloseButtonOnMobile']);
+    }
+    if(isset($input['hideBarsDelay'])) {
+      $valid_input['hideBarsDelay'] = absint($input['hideBarsDelay']);
+    }
+    if(isset($input['videoMaxWidth'])) {
+      $valid_input['videoMaxWidth'] = absint($input['videoMaxWidth']);
+    }
+    if(isset($input['vimeoColor'])) {
 
-        if(isset($input['useSVG']))
-            $valid_input['useSVG'] = (bool)($input['useSVG']);
+      $vimeo_color = strip_tags(stripslashes($input['vimeoColor']));
 
-        if(isset($input['removeBarsOnMobile']))
-            $valid_input['removeBarsOnMobile'] = (bool)($input['removeBarsOnMobile']);
+      //Check if is a valid hex color
+      if(FALSE === $this->checkColor( $vimeo_color)) {
 
-        if(isset( $input['hideCloseButtonOnMobile']))
-            $valid_input['hideCloseButtonOnMobile'] = (bool)($input['hideCloseButtonOnMobile']);
+        // Set the error message
+        add_settings_error('easySwipeBox_lightbox', 'easySwipebox_vimeo_error', __('Insert a valid color for Vimeo controllers', $this->plugin_name), 'error'); // $setting, $code, $message, $type
 
-        if(isset($input['hideBarsDelay']))
-            $valid_input['hideBarsDelay'] = absint($input['hideBarsDelay']);
+        // Get the previous valid value
+        $valid_input['vimeoColor'] = $this->options_lightbox['vimeoColor'];
 
-        if(isset($input['videoMaxWidth']))
-            $valid_input['videoMaxWidth'] = absint($input['videoMaxWidth']);
+      } else {
+        $valid_input['vimeoColor'] = $vimeo_color;
+      }
 
-        if(isset($input['vimeoColor']))
-
-          $vimeo_color = strip_tags(stripslashes($input['vimeoColor']));
-
-          //Check if is a valid hex color
-          if(FALSE === $this->check_color( $vimeo_color)) {
-
-              // Set the error message
-              add_settings_error('easySwipeBox_lightbox', 'easySwipebox_vimeo_error', __('Insert a valid color for Vimeo controllers', $this->plugin_name), 'error'); // $setting, $code, $message, $type
-
-              // Get the previous valid value
-              $valid_input['vimeoColor'] = $this->options_lightbox['vimeoColor'];
-
-          } else {
-              $valid_input['vimeoColor'] = $vimeo_color;
-        }
-
-        if(isset($input['loopAtEnd']))
-            $valid_input['loopAtEnd'] = (bool)($input['loopAtEnd']);
-
-        if(isset($input['autoplayVideos']))
-            $valid_input['autoplayVideos'] = (bool)($input['autoplayVideos']);
-
-        return $valid_input;
+    if(isset($input['loopAtEnd'])) {
+      $valid_input['loopAtEnd'] = (bool)($input['loopAtEnd']);
+    }
+    if(isset($input['autoplayVideos'])) {
+      $valid_input['autoplayVideos'] = (bool)($input['autoplayVideos']);
+    }
+    return $valid_input;
   }
+}
 
   /**
    * Check if color is a valid HEX.
@@ -572,7 +573,7 @@ class EasySwipeboxAdmin {
    * @since    1.1
    * @access   public
    */
-  public function check_color($value) {
+  public function checkColor($value) {
 
       if (preg_match( '/^#?(?:[0-9a-f]{3}){1,2}$/i', $value) ) { // if user insert a HEX color with #
           return true;
